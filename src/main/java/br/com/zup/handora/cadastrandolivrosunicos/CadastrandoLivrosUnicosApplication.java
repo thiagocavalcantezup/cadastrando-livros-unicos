915339f21355321f05d1aaa7a7ddd14fd9ca62a6
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-25 12:30:08 -0300

    Initial commit

---
+++ src/main/java/br/com/zup/handora/cadastrandolivrosunicos/CadastrandoLivrosUnicosApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.cadastrandolivrosunicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrandoLivrosUnicosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastrandoLivrosUnicosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/zup/handora/cadastrandolivrosunicos/CadastrandoLivrosUnicosApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.zup.handora.cadastrandolivrosunicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrandoLivrosUnicosApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
